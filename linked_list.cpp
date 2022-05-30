7a7ec71a15157432f84a304881510fb6b531aa56
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2022-05-31 00:47:15 +0200

    initialized linked list

---
+++ linked_list.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void printList(Node* n)
+{
+    while (n != NULL) {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+  
+    // allocate 3 nodes in the heap
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 5;
+    head->next = second;
+
+    second->data = 7;
+    second->next = third;
+
+    third->data = 9;
+
+    printList(head);
+}
